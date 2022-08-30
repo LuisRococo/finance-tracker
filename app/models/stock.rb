4c0f180631820c3558cbe8e5bf54b0043338b51a
Luis Roberto <luisrobertopenav@gmail.com>
2022-08-30 13:06:11 -0700

    Add api credentials

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,2 +1,8 @@
 class Stock < ApplicationRecord
+  def self.new_lookup(ticker_symbol)
+    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:api_key],
+                                  endpoint: 'https://cloud.iexapis.com/v1')
+    result = client.quote(ticker_symbol)
+    result.to_h['iex_realtime_price']
+  end
 end
