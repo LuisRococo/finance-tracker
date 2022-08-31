465d95f1a13182ae0770a1745959e3e27e080216
Luis Roberto <luisrobertopenav@gmail.com>
2022-08-31 12:48:37 -0700

    Fix stock model attr typo and seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+user_1 = User.create(email: 'root@root.com', password: 'rootro')
+user_2 = User.create(email: 'luis@luis.com', password: 'luis123')
+user_3 = User.create(email: 'pepe@pepe.com', password: 'pepe123')
+
+stock_1 = Stock.create(ticker: 'AAPL', name: 'APPLE')
+stock_2 = Stock.create(ticker: 'GOOG', name: 'GOOGLE')
+stock_3 = Stock.create(ticker: 'MSFT', name: 'MICROSOFT')
+
+user_1.stocks << stock_1
+user_1.stocks << stock_2
+
+user_2.stocks << stock_1
+user_2.stocks << stock_3
+
+user_3.stocks << stock_2
+user_3.stocks << stock_3
