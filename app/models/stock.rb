1f79451f97d70efdc6f8fb4dc0c4275f99a71f79
Luis Roberto <luisrobertopenav@gmail.com>
2022-08-30 17:57:32 -0700

    Add searched stock info on portfolio page

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,8 +1,14 @@
 class Stock < ApplicationRecord
+
   def self.new_lookup(ticker_symbol)
-    client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:api_key],
-                                  endpoint: 'https://cloud.iexapis.com/v1')
-    result = client.quote(ticker_symbol)
-    result.to_h['iex_realtime_price']
+    client = IEX::Api::Client.new(
+      publishable_token: Rails.application.credentials.iex_client[:api_key],
+      secret_token: Rails.application.credentials.iex_client[:secret_token],
+      endpoint: 'https://cloud.iexapis.com/v1'
+    )
+
+    response = client.quote(ticker_symbol)
+    response.to_a
+    new(ticker: ticker_symbol, name: response["company_name"], last_price: response["iex_realtime_price"])
   end
 end
