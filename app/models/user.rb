a80e41c40ab0f226331ecadb3a5668dd7500c91c
Luis Roberto <luisrobertopenav@gmail.com>
2022-09-02 14:23:00 -0700

    Search user now shows results

--- app/models/user.rb
+++ app/models/user.rb
@@ -30,7 +30,7 @@ class User < ApplicationRecord
 
   def self.search(param)
     param.strip!
-    to_send_back = (fisrt_name_matches(param) + last_name_matches(param) + email_matches(name)).unique
+    to_send_back = (fisrt_name_matches(param) + last_name_matches(param) + email_matches(param)).uniq
     return nil unless to_send_back
     to_send_back
   end
@@ -49,5 +49,9 @@ class User < ApplicationRecord
 
   def self.matches(field_name, param)
     where("#{field_name} like ?", "%#{param}%")
+  end
+
+  def except_current_user(users)
+    users.reject { |user| user.id == self.id }
   end
 end
